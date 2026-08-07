c222faba6ca1cadbdec69b56868537d2b0d778d8
Md Ajmal Hussain <ajmalashrafi2016@gmail.com>
2026-08-07 20:17:29 +0530

    New module model defined

---
+++ backend/models/module.model.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const moduleSchema = new mongoose.Schema({
+  courseId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Course',
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  order: {
+    type: Number,
+    required: true,
+  },
+}, { timestamps: true });
+
+export const Module = mongoose.model('Module', moduleSchema);
