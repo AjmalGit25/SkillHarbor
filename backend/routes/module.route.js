651c27605222d2d3275f2b274d59590ebdccfcf8
Md Ajmal Hussain <ajmalashrafi2016@gmail.com>
2026-08-08 22:50:07 +0530

    Progress routes were added

--- backend/routes/module.route.js
+++ backend/routes/module.route.js
@@ -3,7 +3,9 @@ import {
   createModule, updateModule, deleteModule, getModulesByCourse,
   createLesson, updateLesson, deleteLesson, getLessonsByModule,
 } from '../controllers/module.controller.js';
+import { getProgress, markLessonComplete, generateCertificate } from '../controllers/progress.controller.js';
 import adminMiddleware from '../middlewares/admin.mid.js';
+import userMiddleware from '../middlewares/user.mid.js';
 
 const router = express.Router();
 
@@ -11,12 +13,17 @@ const router = express.Router();
 router.post('/course/:courseId/modules',        adminMiddleware, createModule);
 router.put('/modules/:moduleId',                adminMiddleware, updateModule);
 router.delete('/modules/:moduleId',             adminMiddleware, deleteModule);
-router.get('/course/:courseId/modules',         adminMiddleware, getModulesByCourse);
+router.get('/course/:courseId/modules',         getModulesByCourse);
 
 // Lesson routes
 router.post('/modules/:moduleId/lessons',       adminMiddleware, createLesson);
 router.put('/lessons/:lessonId',                adminMiddleware, updateLesson);
 router.delete('/lessons/:lessonId',             adminMiddleware, deleteLesson);
-router.get('/modules/:moduleId/lessons',        adminMiddleware, getLessonsByModule);
+router.get('/modules/:moduleId/lessons',        getLessonsByModule);
+
+// Progress routes
+router.get('/progress/:courseId',                         userMiddleware, getProgress);
+router.post('/progress/:courseId/complete/:lessonId',     userMiddleware, markLessonComplete);
+router.post('/certificate/:courseId',                     userMiddleware, generateCertificate);
 
 export default router;
