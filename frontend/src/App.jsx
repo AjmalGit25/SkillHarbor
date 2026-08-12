d1bc016f6be46ced1cd305ee0697207f6b74d6be
Md Ajmal Hussain <ajmalashrafi2016@gmail.com>
2026-08-12 21:21:54 +0530

    Footer design implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
+  const location = useLocation();
+
   return (
-    <Outlet/>
+    <>
+      <Outlet />
+    </>
   );
 }
 
